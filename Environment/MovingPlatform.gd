39d8a0db7ae09cd415386f27efa2d8645a98cbcd
Viacheslav Popov <qustaine@gmail.com>
2022-04-10 13:22:28 +0100

    Lines on moving platforms added

--- Environment/MovingPlatform.gd
+++ Environment/MovingPlatform.gd
@@ -1,15 +1,18 @@
+tool
 extends KinematicBody2D
 
 const IDLE_DURATION = 0.1
 
 export var move_offset = Vector2.RIGHT * 100
 export var duration = 3.0
+export var is_turned = false   #if turned on -90 degrees
 
 onready var tween = $MoveTween
 onready var start_position = position
 
 func _ready():
-	set_tween(start_position, start_position + move_offset)
+	if !Engine.is_editor_hint():
+		set_tween(start_position, start_position + move_offset)
 	
 func set_tween(from, to):
 	tween.interpolate_property(self, "position", from, to, duration, 
@@ -18,5 +21,9 @@ func set_tween(from, to):
 		Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, duration + IDLE_DURATION * 2)
 	tween.start()
 
-func _on_MoveTween_tween_all_completed():
-	set_tween(start_position, start_position + move_offset)
+func _draw():
+	if Engine.is_editor_hint():
+		if is_turned:
+			draw_line(Vector2.ZERO, Vector2(-move_offset.y, -move_offset.x), Color.lime, 2)
+		else:
+			draw_line(Vector2.ZERO, move_offset, Color.lime, 2)
--- Player/Player.gd
+++ Player/Player.gd
@@ -7,80 +7,88 @@ export var jump_force = 400
 export var jump_decrease_on_release_coefficient = 2
 export var slowdown_coefficient_ground = 0.5
 export var slowdown_coefficient_air = 0.05
-export var boots_active = false
 
 var velocity = Vector2.ZERO
+var boots_active = false
 
 onready var sprite = $Sprite
 
 func _physics_process(delta):
 	if Input.is_action_just_pressed("activate_boots"):
-		if boots_active: boots_active = false
-		else: boots_active = true
-	
+		boots_active = !boots_active
+		
+	movement(delta)
+
+func movement(delta):
 	if boots_active:
-		rotation_degrees = -90
-		velocity.x += gravity * delta
-		
-		var apply_friction = false
-		
-		if Input.is_action_pressed("ui_up"):
-			if velocity.y > 0:
-				velocity.y *= (1 - slowdown_coefficient_ground)   #helps to change direction faster
-			velocity.y = max(velocity.y - acceleration * delta, -max_speed)
-			sprite.flip_h = false
-		elif Input.is_action_pressed("ui_down"):
-			if velocity.y < 0:
-				velocity.y *= (1 - slowdown_coefficient_ground)
-			velocity.y = min(velocity.y + acceleration * delta, max_speed)
-			sprite.flip_h = true
-		else:
-			apply_friction = true
-		
-		if is_on_floor():
-			if Input.is_action_just_pressed("jump"):
-				velocity.x -= jump_force
-			if apply_friction:
-				velocity.y = lerp(velocity.y, 0, slowdown_coefficient_ground)
-		else:
-			if Input.is_action_just_released("jump"):
-				if velocity.x < 0:
-					velocity.x /= jump_decrease_on_release_coefficient
-			
-		if not is_on_floor() and apply_friction:
-			velocity.y = lerp(velocity.y, 0, slowdown_coefficient_air)
-		
-		velocity = move_and_slide(velocity, Vector2.LEFT)
+		active_boots_movement(delta)
 	else:
-		rotation_degrees = 0
-		velocity.y += gravity * delta
-		
-		var apply_friction = false
-		
-		if Input.is_action_pressed("ui_right"):
+		unactive_boots_movement(delta)
+
+func active_boots_movement(delta):
+	rotation_degrees = -90
+	velocity.x += gravity * delta
+	
+	var apply_friction = false
+	
+	if Input.is_action_pressed("ui_up"):
+		if velocity.y > 0:
+			velocity.y *= (1 - slowdown_coefficient_ground)   #helps to change direction faster
+		velocity.y = max(velocity.y - acceleration * delta, -max_speed)
+		sprite.flip_h = false
+	elif Input.is_action_pressed("ui_down"):
+		if velocity.y < 0:
+			velocity.y *= (1 - slowdown_coefficient_ground)
+		velocity.y = min(velocity.y + acceleration * delta, max_speed)
+		sprite.flip_h = true
+	else:
+		apply_friction = true
+	
+	if is_on_floor():
+		if Input.is_action_just_pressed("jump"):
+			velocity.x -= jump_force
+		if apply_friction:
+			velocity.y = lerp(velocity.y, 0, slowdown_coefficient_ground)
+	else:
+		if Input.is_action_just_released("jump"):
 			if velocity.x < 0:
-				velocity.x *= (1 - slowdown_coefficient_ground)   #helps to change direction faster
-			velocity.x = min(velocity.x + acceleration * delta, max_speed)
-			sprite.flip_h = false
-		elif Input.is_action_pressed("ui_left"):
-			if velocity.x > 0:
-				velocity.x *= (1 - slowdown_coefficient_ground)
-			velocity.x = max(velocity.x - acceleration * delta, -max_speed)
-			sprite.flip_h = true
-		else:
-			apply_friction = true
+				velocity.x /= jump_decrease_on_release_coefficient
 		
-		if is_on_floor():
-			if Input.is_action_just_pressed("jump"):
-				velocity.y -= jump_force
-			if apply_friction:
-				velocity.x = lerp(velocity.x, 0, slowdown_coefficient_ground)
-		else:
-			if Input.is_action_just_released("jump"):
-				if velocity.y < 0:
-					velocity.y /= jump_decrease_on_release_coefficient
-			
-		if not is_on_floor() and apply_friction:
-			velocity.x = lerp(velocity.x, 0, slowdown_coefficient_air)
+	if not is_on_floor() and apply_friction:
+		velocity.y = lerp(velocity.y, 0, slowdown_coefficient_air)
+	
+	velocity = move_and_slide(velocity, Vector2.LEFT)
+
+func unactive_boots_movement(delta):
+	rotation_degrees = 0
+	velocity.y += gravity * delta
+	
+	var apply_friction = false
+	
+	if Input.is_action_pressed("ui_right"):
+		if velocity.x < 0:
+			velocity.x *= (1 - slowdown_coefficient_ground)   #helps to change direction faster
+		velocity.x = min(velocity.x + acceleration * delta, max_speed)
+		sprite.flip_h = false
+	elif Input.is_action_pressed("ui_left"):
+		if velocity.x > 0:
+			velocity.x *= (1 - slowdown_coefficient_ground)
+		velocity.x = max(velocity.x - acceleration * delta, -max_speed)
+		sprite.flip_h = true
+	else:
+		apply_friction = true
+	
+	if is_on_floor():
+		if Input.is_action_just_pressed("jump"):
+			velocity.y -= jump_force
+		if apply_friction:
+			velocity.x = lerp(velocity.x, 0, slowdown_coefficient_ground)
+	else:
+		if Input.is_action_just_released("jump"):
+			if velocity.y < 0:
+				velocity.y /= jump_decrease_on_release_coefficient
 		
-		velocity = move_and_slide(velocity, Vector2.UP)
+	if not is_on_floor() and apply_friction:
+		velocity.x = lerp(velocity.x, 0, slowdown_coefficient_air)
+	
+	velocity = move_and_slide(velocity, Vector2.UP)
