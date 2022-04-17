7d0b83e58b65b66e6a67fee13a85110df1ab5e0a
Viacheslav Popov <qustaine@gmail.com>
2022-04-17 13:54:00 +0100

    Kosmo animations added

--- Player/Player.gd
+++ Player/Player.gd
@@ -8,9 +8,9 @@ export var jump_force = 400
 export var jump_decrease_on_release_coefficient = 2
 
 export var slowdown_coefficient_ground = 0.8
-export var slowdown_coefficient_air = 0.05
+export var slowdown_coefficient_air = 0.07
 
-export var dash_force = 220
+export var dash_force = 250
 export var dash_duration = 0.2
 
 var velocity = Vector2.ZERO
@@ -20,15 +20,23 @@ var dash_direction = Vector2.ZERO
 var can_dash = false
 var dashing = false
 
+var attacking = false
+
 onready var sprite = $Sprite
 onready var stats = PlayerStats
+onready var animationTree = $AnimationTree
 
 func _ready():
 	stats.connect("no_health", self, "death")
+	animationTree.active = true
 
 func _physics_process(delta):
 	if Input.is_action_just_pressed("activate_boots"):
 		boots_active = !boots_active
+		
+	if Input.is_action_just_pressed("attack"):
+		animationTree.set("parameters/State/current", 2)
+		attacking = true
 	
 	dash()
 	if !dashing:
@@ -51,6 +59,7 @@ func dash():
 		
 	if Input.is_action_just_pressed("dash") and can_dash and !is_on_floor():
 		if dash_direction != Vector2.ZERO:
+			animationTree.set("parameters/State/current", 3)
 			velocity = dash_direction.normalized() * dash_force
 			can_dash = false
 			dashing = true
@@ -62,6 +71,20 @@ func dash():
 		velocity = move_and_slide(velocity)
 
 func movement(delta):
+	if is_on_floor() and !attacking:
+		animationTree.set("parameters/State/current", 0)
+	elif !attacking:
+		animationTree.set("parameters/State/current", 1)
+	
+	if (Input.is_action_pressed("ui_up") or Input.is_action_pressed("ui_down") or
+		Input.is_action_pressed("ui_left") or Input.is_action_pressed("ui_right")):
+			animationTree.set("parameters/Movement/current", 1)
+	else:
+		animationTree.set("parameters/Movement/current", 0)
+		
+	if Input.is_action_just_pressed("jump"):
+		animationTree.set("parameters/InAir/current", 0)
+
 	if boots_active:
 		active_boots_movement(delta)
 	else:
@@ -77,12 +100,12 @@ func active_boots_movement(delta):
 		if velocity.y > 0:
 			velocity.y *= (1 - slowdown_coefficient_ground)   #helps to change direction faster
 		velocity.y = max(velocity.y - acceleration * delta, -max_speed)
-		sprite.flip_h = false
+		sprite.flip_h = true
 	elif Input.is_action_pressed("ui_down"):
 		if velocity.y < 0:
 			velocity.y *= (1 - slowdown_coefficient_ground)
 		velocity.y = min(velocity.y + acceleration * delta, max_speed)
-		sprite.flip_h = true
+		sprite.flip_h = false
 	else:
 		apply_friction = true
 	
@@ -111,12 +134,12 @@ func unactive_boots_movement(delta):
 		if velocity.x < 0:
 			velocity.x *= (1 - slowdown_coefficient_ground)   #helps to change direction faster
 		velocity.x = min(velocity.x + acceleration * delta, max_speed)
-		sprite.flip_h = false
+		sprite.flip_h = true
 	elif Input.is_action_pressed("ui_left"):
 		if velocity.x > 0:
 			velocity.x *= (1 - slowdown_coefficient_ground)
 		velocity.x = max(velocity.x - acceleration * delta, -max_speed)
-		sprite.flip_h = true
+		sprite.flip_h = false
 	else:
 		apply_friction = true
 	
@@ -142,3 +165,10 @@ func death():
 func _on_Hurtbox_area_entered(area):
 	stats.health -= area.damage
 	area.recharge()
+
+func _on_attack_animation_ended():
+	attacking = false
+
+func _on_air_animation_ended():
+	animationTree.set("parameters/InAir/current", 1)
+	
