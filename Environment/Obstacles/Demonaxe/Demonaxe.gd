e89f0fdcf37c70e4d0d974531b1c69017b0c36ec
Viacheslav Popov <qustaine@gmail.com>
2022-05-20 08:54:35 +0100

    Demonaxe added

---
+++ Environment/Obstacles/Demonaxe/Demonaxe.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+export var pieces_quantity = 5
+export var speed = 3
+export var bound_degree = 180
+
+var piece = preload("res://Environment/Obstacles/Demonaxe/ChainPiece.tscn")
+var end_piece = preload("res://Environment/Obstacles/Demonaxe/ChainEnd.tscn")
+
+onready var sprite = $Sprite
+
+var sinArgument = 0
+
+func _ready():
+	var spawn_point = Vector2(0, -piece.instance().get_rect().size.y / 2)
+	for i in range(0, pieces_quantity):
+		var new_piece = piece.instance()
+		sprite.add_child(new_piece)
+		new_piece.position = spawn_point
+		spawn_point.y -= new_piece.get_rect().size.y
+		
+	var last_piece = end_piece.instance()
+	sprite.add_child(last_piece)
+	spawn_point.y += piece.instance().get_rect().size.y - 20
+	last_piece.position = spawn_point
+	position += spawn_point
+	sprite.position -= spawn_point
+
+func _physics_process(delta):
+	rotation = sin(sinArgument) * PI * bound_degree / 360
+	sinArgument += delta * speed
