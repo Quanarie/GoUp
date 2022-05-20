71cb53e06ffe7216ca6392661c95a10615dec920
Viacheslav Popov <qustaine@gmail.com>
2022-05-20 14:50:24 +0100

    Demonaxe movement added

--- Environment/Obstacles/Demonaxe/Demonaxe.gd
+++ Environment/Obstacles/Demonaxe/Demonaxe.gd
@@ -2,10 +2,11 @@ extends Node2D
 
 export var pieces_quantity = 5
 export var speed = 3
-export var bound_degree = 180
+export var bound_degree = 0
 
 onready var sprite = $Sprite
 onready var hitbox = $Hitbox
+onready var offset = Vector2(0, piece.instance().get_rect().size.y * pieces_quantity - 45)
 
 var piece = preload("res://Environment/Obstacles/Demonaxe/ChainPiece.tscn")
 var end_piece = preload("res://Environment/Obstacles/Demonaxe/ChainEnd.tscn")
@@ -24,7 +25,7 @@ func _ready():
 	sprite.add_child(last_piece)
 	spawn_point.y += piece.instance().get_rect().size.y - 20
 	last_piece.position = spawn_point
-	position += spawn_point
+	position -= offset
 	for child in get_children():
 		child.position -= spawn_point
 
