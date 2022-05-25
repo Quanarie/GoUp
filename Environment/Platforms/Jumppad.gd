e1f96a5f34bce3fd40e1e8906828744a6b36f3a1
unknown <kos04kos@gmail.com>
2022-05-25 19:36:22 +0200

    Pillow added

--- Environment/Platforms/Jumppad.gd
+++ Environment/Platforms/Jumppad.gd
@@ -2,6 +2,18 @@ extends KinematicBody2D
 
 export var jump_force = Vector2(0, 200)
 
+onready var animator = $AnimationPlayer
+
+var velocity = Vector2.ZERO
+
+func _ready():
+	animator.play("idle")
+
 func _on_Area2D_body_entered(body):
 	if body == Globals.player:
 		body.apply_force(jump_force)
+		animator.play("apart")
+	
+func _on_AnimationPlayer_animation_finished(apart):
+	animator.play("idle")
+	
